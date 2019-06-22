07c39df0965d883017747a6e0d8ccaee1b070adf
Александр Слаквич <aleksandr.mxr@gmail.com>
2019-06-22 20:35:03 +0300

    11

---
+++ List2/LinkedList.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List2
+{
+    class Node
+    {
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+        public object Value { get; set; }
+
+        public Node(object value)
+        {
+            Value = value;
+        }
+    }
+    class LinkedList
+    {
+        public Node First { get; private set; }
+        public Node Last { get; private set; }
+        public int Count { get; private set; }
+
+
+        public void AddLast(object newItem)
+        {
+            Count++;
+            if (First == null)
+            {
+                First = Last = new Node(newItem);
+            }
+            else
+            {
+                Node node = new Node(newItem);
+                Last.Next = node;
+                node.Previous = Last;
+                Last = node;
+            }
+        }
+
+        public void AddFirst(object newItem)
+        {
+            Count++;
+            if (First == null)
+            {
+                First = Last = new Node(newItem);
+            }
+            else
+            {
+                Node node = new Node(newItem);
+                First.Next = First;
+                node.Previous = First;
+                First = node;
+            }
+        }
+
+        public void Remove(object newItem)
+        {
+            if (First == null)
+                return;
+            if (Last == First && newItem.Equals(First))
+            {
+                First = Last = null;
+                Count--;
+            }
+            else if (First.Value.Equals(newItem))
+            {
+                First.Previous = null;
+                First = First.Next;
+                Count--;
+            }
+            else if (Last.Value.Equals(newItem))
+            {
+                Last = Last.Previous;
+                Count--;
+            }
+            else
+            {
+                Node current = First;
+                do
+                {
+                    if (current.Value.Equals(newItem))
+                    {
+                        var previous = current.Previous;
+                        var next = current.Next;
+
+                        previous.Next = next;
+                        next.Previous = previous;
+                        Count--;
+                    }
+                    else
+                        current = First.Next;
+                }
+                while (current != null);
+
+            }
+
+        }
+
+        public object RemoveLast()
+        {
+            object removed = Last;
+            Last.Previous = Last;
+            Last.Next = null;
+            Count--;
+            return removed;
+        }
+
+        public object RemoveFirst()
+        {
+            object removed = First;
+            First.Previous = null;
+            First = First.Next;
+            Count--;
+            return removed;
+        }
+
+        public bool Contains(object newObj)
+        {
+            Node current = First;
+            do
+            {
+                if (current.Value.Equals(newObj))
+                    return true;
+                current = First.Next;
+            }
+            while (current != null);
+            return false;
+        }
+
+        public object[] ToArray()
+        {
+            object[] newArr = new object[Count];
+            Node current = First;
+            int i = 0;
+            do
+            {
+                newArr[i] = First;
+                current = First.Next;
+                i++;
+            }
+            while (current != null);
+            return newArr;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+            First = null;
+            Last = null;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < Count; i++)
+            {
+                stringBuilder.AppendLine($"LinkedList{i} = {First}");
+                First = First.Next;
+            }
+            return stringBuilder.ToString();
+        }
+    }
+
+    class Queue
+    {
+        private LinkedList linkedList = new LinkedList();
+
+        public void Enqueue(object newItem)
+        {
+            linkedList.AddFirst(newItem);
+        }
+
+        public object Dequeue()
+        {
+            return linkedList.RemoveFirst();
+        }
+
+        public void Clear()
+        {
+            linkedList.Clear();
+        }
+
+        public object ToArray()
+        {
+            return linkedList.ToArray();
+        }
+
+        public bool Contains(object newObj)
+        {
+            return linkedList.Contains(newObj);
+        }
+
+        public object Peek()
+        {
+            if (linkedList.First == null)
+                return null;
+            return linkedList.First;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int i = 0;
+            Node current = linkedList.First;
+            do
+            {
+                stringBuilder.AppendLine($"Queue{i} = {current}");
+                current = linkedList.First.Next;
+                i++;
+            }
+            while (current != null);
+            return stringBuilder.ToString();
+        }
+    }
+
+    class Stack
+    {
+        private LinkedList stack = new LinkedList();
+
+        public void Clear()
+        {
+            stack.Clear();
+        }
+
+        public bool Contains(object newObj)
+        {
+            return stack.Contains(newObj);
+        }
+
+        public object ToArray()
+        {
+            return stack.ToArray();
+        }
+
+        public object Peek()
+        {
+            if (stack.Last == null)
+                return null;
+            return stack.Last;
+        }
+
+        public void Push(object newObj)
+        {
+            stack.AddLast(newObj);
+        }
+
+        public object Pop()
+        {
+            return stack.RemoveLast();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int i = 0;
+            Node current = stack.First;
+            do
+            {
+                stringBuilder.AppendLine($"Stack{i} = {current}");
+                current = stack.First.Next;
+                i++;
+            }
+            while (current != null);
+            return stringBuilder.ToString();
+        }
+    }
+
+    public class NodeBT
+    {
+        public int Data { get; private set; }
+        public NodeBT Left { get; set; }
+        public NodeBT Right { get; set; }
+
+        public NodeBT(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    class BinaryTree
+    {
+        public NodeBT Root { get; private set; }
+        public int Count { get; private set; }
+
+        public BinaryTree()
+        {
+            Root = null;
+            Count = 0;
+        }
+
+        public BinaryTree(int data)
+        {
+            Root = new NodeBT(data);
+        }
+
+        public void Add(int data)
+        {
+            if (Root == null)
+            {
+                NodeBT newNode = new NodeBT(data);
+                Root = newNode;
+                Count++;
+                return;
+            }
+
+            NodeBT currentNode = Root;
+            bool addData = false;
+            do
+            {
+                if (data < currentNode.Data)
+                {
+                    if (currentNode.Left == null)
+                    {
+                        NodeBT newNode = new NodeBT(data);
+                        currentNode.Left = newNode;
+                        addData = true;
+                        Count++;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.Left;
+                    }
+                }
+                if (data > currentNode.Data)
+                {
+                    if (currentNode.Right == null)
+                    {
+                        NodeBT newNode = new NodeBT(data);
+                        currentNode.Right = newNode;
+                        addData = true;
+                        Count++;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.Right;
+                    }
+                }
+            }
+            while (!addData);
+        }
+
+        public bool Contains(int data)
+        {
+            NodeBT currentNode = Root;
+            bool containsData = false;
+            do
+            {
+                if (currentNode.Data == data)
+                {
+                    containsData = true;
+                    return containsData;
+                }
+                if (data < currentNode.Data)
+                {
+                    currentNode = currentNode.Left;
+                }
+                if (data > currentNode.Data)
+                {
+                    currentNode = currentNode.Right;
+                }
+            }
+            while (!containsData);
+            return containsData;
+        }
+
+        public int[] ToArray()
+        {
+            int[] newArr = new int[Count];
+            NodeBT current = Root;
+            newArr[0] = Root.Data;
+            int i = 1;
+            do
+            {
+                if (current.Data!=Root.Data)
+                newArr[i] = current.Data;
+                i++;
+                if (current.Left != null)
+                    current = current.Left;
+                else if (current.Right != null)
+                    current = current.Right;
+                else if (current.Left == null && current.Right == null)
+                    current = Root;
+            }
+            while (i<Count);
+            return newArr;
+        }
+
+        public void Clear()
+        {
+            Root = null;
+            Count = 0;
+        }
+    }
+}
+
+
--- List2/Program.cs
+++ List2/Program.cs
@@ -6,16 +6,340 @@ using System.Threading.Tasks;
 
 namespace List2
 {
+    class List
+    {
+        private object[] _items = new object[0];
+
+        public int Count { get; private set; }
+
+        public int Capacity => _items.Length;
+
+        public List(int capacity)
+        {
+            Count = capacity;
+            _items = new object[Count];
+        }
+
+        private bool ThrowIfInvalid(int index)
+        {
+            if ((index < 0) || (index >= Count))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Add(object item)
+        {
+            _items = new object[Count + 1];
+            _items[Count] = item;
+        }
+        private void TryResize()
+        {
+            Count++;
+            if (_items.Length < Count)
+            {
+                Array.Resize(ref _items, _items.Length == 0 ? 1 : _items.Length * 2);
+            }
+        }
+
+        private void Insert(int index, object item)
+        {
+            TryResize();
+            for (var i = Count - 1; i > index; i--)
+            {
+                _items[i] = _items[i - 1];
+            }
+            _items[index] = item;
+        }
+
+        public object this[int i]
+        {
+            get
+            {
+                return _items[i];
+            }
+            set
+            {
+                _items[i] = value;
+            }
+        }
+
+        public int IndexOf(object item)
+        {
+            int i = 0;
+            while ((i < Count) && (!_items[i].Equals(item)))
+            {
+                i++;
+            }
+            if (i == Count)
+            {
+                return -1;
+            }
+            return i;
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (var i = index; i < Count - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+            _items[Count - 1] = default(object);
+            Count--;
+        }
+
+        public bool Contains(object item)
+        {
+            bool m = false;
+            for (int i = 0; i < Count - 1; i++)
+            {
+                if (_items[i] == item)
+                    m = true;
+            }
+            return m;
+        }
+
+        public void Clear()
+        {
+            _items = new object[0];
+        }
+
+        public void Remove(object item)
+        {
+            RemoveAt(IndexOf(item));
+        }
+
+        public void Reverse()
+        {
+            _items = _items.Reverse().ToArray();
+        }
+
+        public void Reverse2()
+        {
+            object f = null;
+            for (var i = 0; i < _items.Length - 1; i++)
+            {
+                _items[i] = f;
+                _items[i] = _items[_items.Length - i];
+                _items[_items.Length - i] = f;
+
+            }
+        }
+
+        public object[] ToArray()
+        {
+            object[] newArr = new object[_items.Length];
+            for (int i = 0; i < _items.Length; i++)
+            {
+                newArr[i] = _items[i];
+            }
+            return newArr;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < _items.Length; i++)
+            {
+                stringBuilder.AppendLine($"List{i} = {_items[i]}");
+            }
+            return stringBuilder.ToString();
+        }
+    }
+    public class File
+    {
+        public string Name;
+        public string Size;
+        public string Extention;
+        public string AdditionalInfo;
+        
+        public File[] ArrInput(string input)
+        {
+            string[] arrString = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            File[] arrFiles = new File[arrString.Length];
+            int found = 0;
+            for (int i = 0; i < arrString.Length; i++)
+            {
+                if (arrString[i].StartsWith("Text:"))
+                {
+                    found = arrString[i].IndexOf(":");
+                    arrFiles[i] = new TextFiles(arrString[i].Substring(found + 1));
+                    Print(arrFiles[i]);
+                }
+                else if (arrString[i].StartsWith("Image:"))
+                {
+                    found = arrString[i].IndexOf(":");
+                    arrFiles[i] = new Images(arrString[i].Substring(found + 1));
+                    Print(arrFiles[i]);
+                }
+                else if (arrString[i].StartsWith("Movie:"))
+                {
+                    found = arrString[i].IndexOf(":");
+                    arrFiles[i] = new Movies(arrString[i].Substring(found + 1));
+                    Print(arrFiles[i]);
+                }
+            }
+            return arrFiles;
+        }
+        
+        protected virtual void Parse(string text)
+        {
+            string word = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '(')
+                {
+                    Extention = word;
+                    word = "";
+                }
+                else if (text[i] == ')')
+                {
+                    Size = word;
+                    word = "";
+                }
+                else if (text[i] == '.')
+                {
+                    Name = word;
+                    word = "";
+                }
+                else
+                {
+                    word += text[i];
+                }
+                AdditionalInfo = word;
+            }
+        }
+
+        public virtual void Print(File file)
+        {
+            Console.WriteLine($"{file.Name}.{file.Extention}");
+            Console.WriteLine($"           Extension: {file.Extention}");
+            Console.WriteLine($"           Extension: {file.Size}");
+        }
+        
+        public virtual File [] SortBySize(File [] files)
+        {
+            File temp;
+            for (int i = 0; i < files.Length; i++)
+            {
+                for (int j = i + 1; j < files.Length; j++)
+                {
+                    int m = Convert.ToInt32(files[i].Size);
+                    int n = Convert.ToInt32(files[j].Size);
+                    if ( m > n)
+                    {
+                        temp = files[i];
+                        files[i] = files[j];
+                        files[j] = temp;
+                    }
+                }
+            }
+            return files;
+        }
+
+
+        
+    }
+    public class TextFiles : File
+    {
+        public string Content;
+
+        public TextFiles(string text)
+        {
+            Parse(text);
+            Content = AdditionalInfo;
+        }
+
+        protected override void Parse(string text)
+        {
+            base.Parse(text);
+        }
+
+        public override void Print(File file)
+        {
+            base.Print(file);
+            Console.WriteLine($"           Content: {Content}");
+        }
+    }
+    public class Movies : File
+    {
+        public string Resolution;
+        public string Length;
+
+        public Movies(string text)
+        {
+            Parse(text);
+        }
+
+        protected override void Parse(string text)
+        {
+            base.Parse(text);
+            string word = "";
+            for (int i = 0; i < AdditionalInfo.Length; i++)
+            {
+                
+                if (Extention[i] == ';')
+                {
+                    Resolution = word;
+                    word = "";
+                }
+                else
+                {
+                    word += text[i];
+                }
+                Length = word;
+            }
+        }
+
+        public override void Print(File file)
+        {
+            base.Print(file);
+            Console.WriteLine($"           Resolution: {Resolution}");
+            Console.WriteLine($"           Length: {Length}");
+        }
+    }
+    public class Images : File
+    {
+        public string Resolution;
+        public Images(string text)
+        {
+            Parse(text);
+            Resolution = AdditionalInfo;
+        }
+
+        protected override void Parse(string text)
+        {
+            base.Parse(text);
+        }
+
+        public override void Print(File file)
+        {
+            base.Print(file);
+            Console.WriteLine($"           Resolution: {Resolution}");
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print â€˜Hello Worldâ€™ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            string text = @"        Text: file.txt(6B); Some string content
+                                    Image: img.bmp(19MB); 1920õ1080
+                                    Text: data.txt(12B); Another string
+                                    Text: data1.txt(7B); Yet another string
+                                    Movie:logan.2017.mkv(19GB); 1920õ1080; 2h12m";
+
+            File[] files = new File[5];
+            files.ArrInput(text);
+
+
+
+
         }
+        
     }
 }
+
+
